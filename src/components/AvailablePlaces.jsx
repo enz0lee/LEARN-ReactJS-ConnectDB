95aaa5ac5e5c95add26ec9636d6640dd98a236b3
Enzo Lee <soloenzo.dev@gmail.com>
2025-07-25 18:54:55 +0700

    feat: add loading text

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,35 @@
-import Places from './Places.jsx';
+import { useEffect, useState } from 'react'
+import Places from './Places.jsx'
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsLoading(true)
+      const response = await fetch('http://localhost:3000/places')
+      if (!response.ok) {
+        throw new Error('Failed to fetch places')
+      }
+      const data = await response.json()
+      setAvailablePlaces(data.places)
+    }
+    fetchPlaces()
+      .catch((error) => {
+        console.error('Error fetching places:', error)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }, [])
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isLoading}
+      loadingText="Fetching available places..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
-  );
+  )
 }
